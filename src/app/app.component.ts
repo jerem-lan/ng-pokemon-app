7c4008e24aa743d74f07c3fb23432cda93a75d96
jeremy <jeremy.langlet@gmail.com>
2020-10-27 13:18:54 +0100

    premier pas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
+  
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pokemon-app',
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
-}
+export class AppComponent  { name = 'Angular'; }
