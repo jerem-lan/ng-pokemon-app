25db3decdc6a5a889585a5b0fef81323fe9698b2
jeremy <jeremy.langlet@gmail.com>
2020-10-29 12:11:35 +0100

    add component & class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
   
 @Component({
   selector: 'pokemon-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Pokemons !!</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit { 
+
+  private pokemons: Pokemon[] = null;
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    alert("vous avez selectionné " + pokemon.name);
+  }
+
+}
