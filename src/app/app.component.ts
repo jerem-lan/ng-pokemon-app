86b2c18217f687c5400db7b5a87c96ecbf49852f
jeremy <jeremy.langlet@gmail.com>
2020-11-03 15:49:32 +0100

    add service & change routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemons';
+
   
 @Component({
   selector: 'pokemon-app',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { PokemonsModule } from './pokemons/pokemons.module';
 
 import { AppComponent } from './app.component';
-import { ListPokemonComponent } from './list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe} from './pokemon-type-color.pipe';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    PokemonsModule,
     AppRoutingModule,
     RouterModule
   ],
